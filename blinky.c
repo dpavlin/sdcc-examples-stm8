4760644c2d11ed6a97fe52e9cabc33bacbafd2b2
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-05-21 12:42:29 +0200

    select PD3 pin for LED

--- blinky.c
+++ blinky.c
@@ -1,13 +1,15 @@
 #include "stm8l.h"
 
+#define PIN 0b00001000 // PD3 -- pin 3
+
 int main() {
 	int d;
 	// Configure pins
-	PE_DDR = 0x80;
-	PE_CR1 = 0x80;
+	PD_DDR = PIN;
+	PD_CR1 = PIN;
 	// Loop
 	do {
-		PE_ODR ^= 0x80;
+		PD_ODR ^= PIN;
 		for(d = 0; d < 29000; d++) { }
 	} while(1);
 }
